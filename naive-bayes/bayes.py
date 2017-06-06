a35d7e0c810be534116dbe387028574cba4d7a8a
zzw0929 <zhuzw0929@gmail.com>
2017-06-07 01:16:20 +0800

    add filter garbage in email

--- naive-bayes/bayes.py
+++ naive-bayes/bayes.py
@@ -2,6 +2,7 @@
 #-*- coding:utf-8 -*-
 
 from numpy import *
+import re
 
 def loadDataSet():
     postingList = [
@@ -24,7 +25,7 @@ def createVocabList(dataSet):
     return list(vocabSet)
 
 # 将单词变成一个向量
-def setOfWord2Vec(vocabList, inputSet):
+def setOfWords2Vec(vocabList, inputSet):
 	# 创建一个所含元素都是0的向量
 	returnVec = [0] * len(vocabList)
 	for word in inputSet:
@@ -34,14 +35,24 @@ def setOfWord2Vec(vocabList, inputSet):
 			print "the word: %s is not in my Vocabulary !" %word
 	return returnVec
 
+def bagOfWords2VecMN(vocabList, inputSet):
+	returnVec = [0] * len(vocabList)
+	for word in inputSet:
+		if word in vocabList:
+			returnVec[vocabList.index(word)] += 1
+	return returnVec
+
 def trainNB0(trainMatrix, trainCategory):
 	numTrainDocs = len(trainMatrix)
 	# 侮辱性词汇的数量
 	numWords = len(trainMatrix[0])
 	# 侮辱性句子在总句子中的比例
 	pAubsive = sum(trainCategory) / float(numTrainDocs)
-	p0Num = zeros(numWords);p1Num = zeros(numWords)
-	p0Denom = 0.0; p1Denom = 0.0
+
+	# p0Num = zeros(numWords);p1Num = zeros(numWords)
+	# p0Denom = 0.0; p1Denom = 0.0
+	p0Num = ones(numWords); p1Num = ones(numWords)
+	p0Denom = 2.0; p1Denom = 2.0
 	for i in range(numTrainDocs):
 		# 侮辱性的词汇和正常单词统计
 		# 向量相加
@@ -52,13 +63,75 @@ def trainNB0(trainMatrix, trainCategory):
 		else:
 			p0Num += trainMatrix[i]
 			p0Denom += sum(trainMatrix[i])
-	print "1:", p1Num, p1Denom
-	print "0:", p0Num, p0Denom
+
 	# 侮辱性词汇中每个单词在侮辱性词汇中的概率
-	p1Vect = p1Num / p0Denom
+	# p1Vect = p1Num / p0Denom
 	# 正常词汇中每个单词在正常词汇中的概率
-	p0Vect = p0Num / p0Denom
-	return p1Vect, p0Vect,pAubsive
+	# p0Vect = p0Num / p0Denom
+	p1Vect = log(p1Num / p1Denom)
+	p0Vect = log(p0Num / p0Denom)
+	return p0Vect, p1Vect,pAubsive
+
+def classifyNB(vec2Classify, p0Vect, p1Vect, pClass1):
+	# 元素相乘
+	p1 = sum(vec2Classify * p1Vect) + log(pClass1)
+	p0 = sum(vec2Classify * p0Vect) + log(1.0 - pClass1)
+	if p1 > p0:
+		return 1
+	else:
+		return 0
+
+def testingNB():
+	listOPosts, listClasses = loadDataSet()
+	myVocabList = createVocabList(listOPosts)
+	trainMat = []
+	for postinDoc in listOPosts:
+		trainMat.append(setOfWords2Vec(myVocabList, postinDoc))
+	p0V, p1V, pAb = trainNB0(array(trainMat), array(listClasses))
+	testEntry = ['love', 'my', 'dalmation']
+	thisDoc = array(setOfWords2Vec(myVocabList, testEntry))
+	print testEntry, 'classified as: ', classifyNB(thisDoc, p0V, p1V, pAb)
+	testEntry = ['stupid','garbage']
+	thisDoc = array(setOfWords2Vec(myVocabList, testEntry))
+	print testEntry, 'classified as: ', classifyNB(thisDoc, p0V, p1V, pAb)
+
+#  测试算法
+def textParse(bigString):
+	listOfTokens = re.split(r'\W*', bigString)
+	return [tok.lower() for tok in listOfTokens if len(tok) > 2]
+def spamTest():
+	docList = []; classList = []; fullText = []
+	for i in range(1,26):
+		wordList = textParse(open('email/spam/%d.txt' % i).read())
+		docList.append(wordList)
+		fullText.extend(wordList)
+		classList.append(1)
+		wordList = textParse(open('email/ham/%d.txt' % i).read())
+		docList.append(wordList)
+		fullText.extend(wordList)
+		classList.append(0)
+	vocabList = createVocabList(docList)
+	trainingSet = range(50); testSet = []
+
+	# 随机构建训练集
+	for i in range(10):
+		randIndex = int(random.uniform(0, len(trainingSet)))
+		testSet.append(trainingSet[randIndex])
+		del(trainingSet[randIndex])
+	trainMat = []; trainClass = []
+	for docIndex in trainingSet:
+		trainMat.append(setOfWords2Vec(vocabList, docList[docIndex]))
+		trainClass.append(classList[docIndex])
+	p0V, p1V, pSpam = trainNB0(array(trainMat), array(trainClass))
+	errorCount = 0
+
+	# 对测试集分类
+	for docIndex in testSet:
+		wordVector = setOfWords2Vec(vocabList, docList[docIndex])
+		if classifyNB(array(wordVector), p0V, p1V, pSpam) != classList[docIndex]:
+			errorCount += 1
+	print 'the error rate is: ', float(errorCount) / len(testSet)
+
 
 if __name__ == '__main__':
 	listOPosts, listClasses = loadDataSet()
@@ -67,12 +140,12 @@ if __name__ == '__main__':
 
 	myVocabList = createVocabList(listOPosts)
 	print myVocabList
-	print setOfWord2Vec(myVocabList, listOPosts[0])
-	print setOfWord2Vec(myVocabList, listOPosts[3])
+	print setOfWords2Vec(myVocabList, listOPosts[0])
+	print setOfWords2Vec(myVocabList, listOPosts[3])
 	print '------------------'
 	trainMat = []
 	for postinDoc in listOPosts:
-		vect = setOfWord2Vec(myVocabList,postinDoc)
+		vect = setOfWords2Vec(myVocabList,postinDoc)
 		trainMat.append(vect)
 	print trainMat
 	# trainMat 是个32*5的向量
@@ -83,4 +156,24 @@ if __name__ == '__main__':
 	print p0V
 	print p1V
 	print pAb
+	print '--------------------------------'
+	testingNB()
+	print '-----------test--4.6.1------'
+
+	mySent = 'This book is the best bool on Python or M.L. I have ever laid eyes upon.'
+	print mySent.split()
+	regEx = re.compile('\\W*')
+	# 除掉单词，数字外的任意字符串
+	listOfTokens = regEx.split(mySent)
+	print listOfTokens
+    # 去掉空字符串
+	print [tok.lower() for tok in listOfTokens if len(tok) > 0]
+
+	print '-----------demo-------'
+	emailText = open('email/ham/6.txt').read()
+	print emailText
+	listOfTokens = regEx.split(emailText)
+	print listOfTokens
+
+	spamTest()
 
